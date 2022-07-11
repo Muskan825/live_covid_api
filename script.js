78d6559bb02eda05987dae6437b18457887b99ff
Muskan <85387134+Muskan825@users.noreply.github.com>
2022-07-11 11:04:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const api_url = "https://api.covid19api.com/summary";
+var coun;
+function gen(){  
+    getData(api_url);    
+    var x = document.getElementById("mySelect").selectedIndex;
+    coun =document.getElementsByTagName("option")[x].value;
+    document.getElementById("result").style="display:inline";   
+}
+function getData(url){
+    fetch(url)
+    .then((res) => {   
+        return res.json(); 
+    }).then((data) => displayData(data.Countries))
+    .catch((error) => console.log(error.message));
+}
+function displayData(API_Data){
+    console.log(API_Data);
+    var x = API_Data.length;
+    for(let i=0; i<x;i++){
+        if(API_Data[i].Country == coun){
+            document.getElementById("p1").innerHTML= "<h4 style='display:inline; font-size: 15px;'>Country Name : </h4>"+ coun;
+            document.getElementById("p2").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Date : </h4>" + Date();
+            document.getElementById("p3").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Cases : </h4>"+API_Data[i].TotalConfirmed;
+            document.getElementById("p4").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Deaths : </h4>"+API_Data[i].TotalDeaths;
+            document.getElementById("p5").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Cases : </h4>"+API_Data[i].NewConfirmed;
+            document.getElementById("p6").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Deaths : </h4>"+API_Data[i].NewDeaths;
+        }
+    }
+}
