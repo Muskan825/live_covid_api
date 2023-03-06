c5de18f778a22d36d6a1b6a3a946150e8dbee045
Muskan <85387134+Muskan825@users.noreply.github.com>
2023-03-06 17:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ function displayData(API_Data){
     var x = API_Data.length;
     for(let i=0; i<x;i++){
         if(API_Data[i].Country == coun){
-            document.getElementById("p1").innerHTML= "<h4 style='display:inline; font-size: 15px;'>Country Name</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+ coun;
-            document.getElementById("p2").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Date</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + Date().toDateString();
-            document.getElementById("p3").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Cases</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].TotalConfirmed;
-            document.getElementById("p4").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Death</h4> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].TotalDeaths;
-            document.getElementById("p5").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Cases</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].NewConfirmed;
-            document.getElementById("p6").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Deaths</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].NewDeaths;
+            document.getElementById("p1").innerHTML= "<h4 style='display:inline; font-size: 15px;'>Country : </h4> "+ coun;
+            document.getElementById("p2").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Date : </h4> " + Date();
+            document.getElementById("p3").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Cases : </h4>"+API_Data[i].TotalConfirmed;
+            document.getElementById("p4").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Death : </h4> "+API_Data[i].TotalDeaths;
+            document.getElementById("p5").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Cases : </h4> "+API_Data[i].NewConfirmed;
+            document.getElementById("p6").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Deaths : </h4> "+API_Data[i].NewDeaths;
         }
     }
 }
