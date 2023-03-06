ffd39ae625e67fd2d6682b8ccd796a05183d0e9c
Muskan <85387134+Muskan825@users.noreply.github.com>
2023-03-06 17:02:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function displayData(API_Data){
     for(let i=0; i<x;i++){
         if(API_Data[i].Country == coun){
             document.getElementById("p1").innerHTML= "<h4 style='display:inline; font-size: 15px;'>Country Name</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+ coun;
-            document.getElementById("p2").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Date</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + Date();
+            document.getElementById("p2").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Date</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + Date().toDateString();
             document.getElementById("p3").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Cases</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].TotalConfirmed;
             document.getElementById("p4").innerHTML= "<h4 style='display:inline; font-size: 15px;''>Total Death</h4> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].TotalDeaths;
             document.getElementById("p5").innerHTML= "<h4 style='display:inline; font-size: 15px;''>New Cases</h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+API_Data[i].NewConfirmed;
